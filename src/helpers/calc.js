2116fe929ad3a9055eb574ed45c40de1fd2ad16b
Aurélien Riche <contact@aurelienriche.com>
2020-08-22 17:51:09 +0200

    Fix

--- src/helpers/calc.js
+++ src/helpers/calc.js
@@ -2,5 +2,5 @@ export function isNumber(num) {
     return !isNaN(parseFloat(num)) && isFinite(num);
 }
 export function round2(num) {
-    return this.isNumber(num) ? (Math.round(num * Math.pow(10, 2)) / Math.pow(10, 2)) : 0;
+    return isNumber(num) ? (Math.round(num * Math.pow(10, 2)) / Math.pow(10, 2)) : 0;
 }
